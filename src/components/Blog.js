cb050bdeeeff28503f7956182194b789e63f27dd
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2025-02-10 16:46:53 +0200

    updated

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,43 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './styles/Blog.css';
-import fashion1 from '../assets/fashion1.jpg';  // Import the first image
-import fashion2 from '../assets/fashion2.jpg';  // Import the second image
-import fashion3 from '../assets/fashion3.jpg';  // Import the third image
+import fashion1 from '../assets/fashion1.jpg';
+import fashion2 from '../assets/fashion2.jpg';
+import fashion3 from '../assets/fashion3.jpg';
 
 const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/posts')
+      .then(response => setPosts(response.data))
+      .catch(error => console.error('Error fetching posts:', error));
+  }, []);
+
+  // Array of images to assign randomly to posts (if no image field is in backend)
+  const images = [fashion1, fashion2, fashion3];
+
   return (
     <div className="blog">
       <h2>Latest Fashion Articles</h2>
 
-      {/* Blog Post 1 */}
-      <div className="blog-post">
-        <img src={fashion1} alt="Fashion Post 1" />  {/* First image */}
-        <div>
-          <h3>How to Style Your Outfits in 2025</h3>
-          <p>Discover the best outfit combinations for this year.</p>
-          <a href="#">Read More</a>
-        </div>
-      </div>
-
-      {/* Blog Post 2 */}
-      <div className="blog-post">
-        <img src={fashion2} alt="Fashion Post 2" />  {/* Second image */}
-        <div>
-          <h3>Top Trends for Spring 2025</h3>
-          <p>Explore the hottest fashion trends for the upcoming season.</p>
-          <a href="#">Read More</a>
-        </div>
-      </div>
-
-      {/* Blog Post 3 */}
-      <div className="blog-post">
-        <img src={fashion3} alt="Fashion Post 3" />  {/* Third image */}
-        <div>
-          <h3>Ultimate Guide to Fall Fashion 2025</h3>
-          <p>Get ready for the best outfits to wear this fall.</p>
-          <a href="#">Read More</a>
-        </div>
-      </div>
+      {posts.length > 0 ? (
+        posts.map((post, index) => (
+          <div className="blog-post" key={post.id}>
+            <img src={images[index % images.length]} alt={`Fashion Post ${index + 1}`} /> 
+            <div>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No blog posts available.</p>
+      )}
     </div>
   );
 };
