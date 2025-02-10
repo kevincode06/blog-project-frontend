bbf9a183f79c2ae4b90d8712d4b6b31bb54eb6b7
kevincode06 <113902012+kevincode06@users.noreply.github.com>
2025-02-10 08:54:13 +0200

    login

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/Auth.css";
+
+const Login = () => {
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+      <form className="auth-form">
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+      <p>Don't have an account? <a href="/signup">Sign up</a></p>
+    </div>
+  );
+};
+
+export default Login;
